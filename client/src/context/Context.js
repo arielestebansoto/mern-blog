3aa459f2ae5880ec24d1020c953c31d20b19cbcc
Ariel Esteban Soto <arielestebansoto@gmail.com>
2021-12-24 11:19:54 -0300

    login system and uning local Storage

--- client/src/context/Context.js
+++ client/src/context/Context.js
@@ -1,8 +1,8 @@
-import { createContext, useReducer } from "react"
+import React, { createContext, useReducer } from "react"
 import { Reducer } from "./Reducer"
 
 const INITIAL_STATE = {
-    user: null,
+    user: JSON.parse(localStorage.getItem('user')) || null,
     isFetching: false,
     error: false,
 }
@@ -11,6 +11,11 @@ export const Context = createContext(INITIAL_STATE)
 
 export const ContextProvider = ({ children }) => {
     const [ state, dispatch ] = useReducer(Reducer, INITIAL_STATE)
+    
+    React.useEffect( () => {
+        localStorage.setItem("user", JSON.stringify(state.user))
+    }, [state])
+    
     return (
         <Context.Provider 
             value={{
--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,24 +1,60 @@
-import './styles.css'
+import React from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 
+import './styles.css'
+
+import { Context } from '../../context/Context'
+import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAILURE } from '../../context/actionsTypes'
+
 export const Login = () => {
+
+    const userRef = React.useRef()
+    const passwordRef = React.useRef()
+    const { dispatch, isFetching } = React.useContext(Context)
+    
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        dispatch({type: LOGIN_START })
+        try {
+            const res = await axios.post('/auth/login', {
+                username: userRef.current.value,
+                password: passwordRef.current.value
+            })            
+            dispatch({
+                type: LOGIN_SUCCESS,
+                payload: res.data
+            })
+        } catch (err) {
+            dispatch({type: LOGIN_FAILURE })
+        }
+    }
+
     return (
         <div className="login">
             <span className="loginTitle">Login</span>
-            <form className="loginForm">
-                <label>Email</label>
+            <form className="loginForm" onSubmit={handleSubmit}>
+                <label>Username</label>
                 <input 
                     type="text" 
                     className="loginInput"
-                    placeholder="Enter your email..." 
+                    placeholder="Enter your username..." 
+                    ref={userRef}
                 />
                 <label>Password</label>
                 <input 
                     type="password" 
                     className="loginInput"
                     placeholder="Enter your password..." 
+                    ref={passwordRef}
                 />
-                <button className="loginButton">Login</button>
+                <button 
+                    type="submit" 
+                    className="loginButton"
+                    disabled={isFetching}
+                >
+                    Login
+                </button>
             </form>
                 <button 
                     className="loginRegisterButton"
